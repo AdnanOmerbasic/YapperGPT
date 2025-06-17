0df491bf5b8e0ad4956750ee8b6df29863271e90
AdnanOmerbasic <AdnanOmerbasic99@gmail.com>
2025-06-17 14:34:39 +0200

    remove unused google redirect uri paths helper

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -10,7 +10,3 @@ export const chatPath = (conversationId: string) => `/chat/${conversationId}`;
 
 // Chat
 export const chatListPath = () => '/chat';
-
-// OAuth - Google
-export const googleOAuth_RedirectURI = () =>
-  `${process.env.GOOGLE_REDIRECT_URI_DEV ?? process.env.GOOGLE_REDIRECT_URI_PROD}`;
